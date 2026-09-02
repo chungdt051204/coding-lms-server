47cc040865b1a5eb0fb1de119f71d9b7e1e1de0d
chungdt051204 <email_github_ca_bn@gmail.com>
2026-09-02 12:20:01 +0700

    Cập nhật lần 1

--- configs/database.js
+++ configs/database.js
@@ -1,7 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/lvtn");
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log("Kết nối thành công");
   } catch (error) {
     console.log("Lỗi kết nối database", { error: error.message });
